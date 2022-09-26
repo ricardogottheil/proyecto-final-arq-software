92cf0e359e8e1a894e49ad282df4d306fd6dea8f
Ricardo Gottheil <ricardogottheil@gmail.com>
2022-09-26 17:56:27 -0500

    Frontend

--- front-end-bicicletas/src/components/navbar.jsx
+++ front-end-bicicletas/src/components/navbar.jsx
@@ -1,5 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
 export const Navbar = () => {
+    const { user, isAuthenticated, logout } = useAuth0();
     return (
         <div className="navbar bg-base-100">
             <div className="flex-1">
@@ -8,29 +12,34 @@ export const Navbar = () => {
             <div className="flex-none">
                 <ul className="menu menu-horizontal p-0">
                     <li>
-                        <a href="/">Inicio</a>
+                        <Link to="/">Inicio</Link>
                     </li>
                     <li>
-                        <a href="/list">Listar bicicletas</a>
+                        <Link to="/list">Listar bicicletas</Link>
                     </li>
                 </ul>
             </div>
 
-            <div className="dropdown dropdown-end">
-                <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                        <img src="https://placeimg.com/80/80/people" />
-                    </div>
-                </label>
-                <ul
-                    tabIndex={0}
-                    className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
-                >
-                    <li>
-                        <a>Logout</a>
-                    </li>
-                </ul>
-            </div>
+            {isAuthenticated && (
+                <div className="dropdown dropdown-end">
+                    <label
+                        tabIndex={0}
+                        className="btn btn-ghost btn-circle avatar"
+                    >
+                        <div className="w-10 rounded-full">
+                            <img src={user.picture} alt={user.name} />
+                        </div>
+                    </label>
+                    <ul
+                        tabIndex={0}
+                        className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+                    >
+                        <li>
+                            <button onClick={logout}>Logout</button>
+                        </li>
+                    </ul>
+                </div>
+            )}
         </div>
     );
 };
